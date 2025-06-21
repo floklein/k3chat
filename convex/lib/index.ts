4f21f4b681f8509eca491258fcb484ad15e060df
floklein <4128942+floklein@users.noreply.github.com>
2025-06-21 13:17:14 +0200

    chat name length

--- convex/lib/index.ts
+++ convex/lib/index.ts
@@ -2,10 +2,10 @@ import type { UserMessage } from "../schema";
 
 export function messageContentToChatName(content: UserMessage["content"]) {
   if (typeof content === "string") {
-    return content.slice(0, 20);
+    return content.slice(0, 50);
   }
   return (
-    content.find((part) => part.type === "text")?.text.slice(0, 20) ??
+    content.find((part) => part.type === "text")?.text.slice(0, 50) ??
     "New Chat"
   );
 }
